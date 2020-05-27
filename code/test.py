ec3ef02d959e7b996b4ade734861d4c7c427c8ad
Luis Soriano <49929267+Laele@users.noreply.github.com>
2020-05-27 12:54:13 -0500

    Update test.py

--- code/test.py
+++ code/test.py
@@ -1,7 +1,6 @@
 #from model.model import Model
 
 #m = Model()
-#print('Ahuevo wey')
 
 #m.create_pelicula("Avengers End-Game","B","Acción","Super Heroes Marvel luchan contra Thanos.","Luis Soriano","2:10:02")
 
